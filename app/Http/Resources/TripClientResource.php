f83e349bf9a14feb6e4faf72c100caa59e45b08f
AbdelaliKDM <khademallah.abdelali@gmail.com>
2026-01-13 17:42:14 +0100

    Refactor TripClientResource to use null-safe operator for client properties

--- app/Http/Resources/TripClientResource.php
+++ app/Http/Resources/TripClientResource.php
@@ -21,18 +21,18 @@ class TripClientResource extends JsonResource
             //'client_id' => $this->client_id,
             //'client_type' => $this->client_type,
             'passenger_id' => $this->client_type == Passenger::class ? $this->client_id : null,
-            'fullname' => $this->client->fullname,
-            'phone' => $this->client->phone,
+            'fullname' => $this->client?->fullname,
+            'phone' => $this->client?->phone,
             'image' => $this->client_type == Passenger::class ? $this->client->getFirstMediaUrl('image') : "",
             'number_of_seats' => $this->number_of_seats,
             'total_fees' => $this->total_fees,
             'note' => $this->note,
             //'created_at' => $this->created_at,
             //'updated_at' => $this->updated_at,
-            
+
             // Include client information
             /* 'client' => $this->whenLoaded('client', function () {
-                return $this->client_type === 'App\Models\Passenger' 
+                return $this->client_type === 'App\Models\Passenger'
                     ? new PassengerResource($this->client)
                     : new GuestResource($this->client);
             }), */
