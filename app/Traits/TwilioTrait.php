5312fbc9596e68eef29440d3e59f1b80e89df927
AbdelaliKDM <khademallah.abdelali@gmail.com>
2026-03-04 18:03:40 +0100

    refactor(TwilioTrait): improve error handling in sendOTP and verifyOTP methods

--- app/Traits/TwilioTrait.php
+++ app/Traits/TwilioTrait.php
@@ -1,32 +1,40 @@
 <?php
 namespace App\Traits;
 
+use Exception;
 use Twilio\Rest\Client;
 
 trait TwilioTrait{
 
   public function sendOTP($toPhoneNumber){
+    try {
+      $client = new Client(getenv("TWILIO_ACCOUNT_SID"), getenv("TWILIO_AUTH_TOKEN"));
+      $verification = $client->verify->v2
+        ->services(getenv("TWILIO_VERIFY_SERVICE_SID"))
+        ->verifications->create(
+          $toPhoneNumber,
+          "sms"
+        );
 
-    $client = new Client(getenv("TWILIO_ACCOUNT_SID"), getenv("TWILIO_AUTH_TOKEN"));
-    $verification = $client->verify->v2
-      ->services(getenv("TWILIO_VERIFY_SERVICE_SID"))
-      ->verifications->create(
-        $toPhoneNumber,
-        "sms"
-      );
-
-    return $verification->status;
+      return $verification->status;
+    } catch (Exception $e) {
+      throw new Exception($e->getMessage(), 424);
+    }
   }
 
   public function verifyOTP($toPhoneNumber, $code){
-    $client = new Client(getenv("TWILIO_ACCOUNT_SID"), getenv("TWILIO_AUTH_TOKEN"));
-    $verification_check = $client->verify->v2
-      ->services(getenv("TWILIO_VERIFY_SERVICE_SID"))
-      ->verificationChecks->create([
-        "to" => $toPhoneNumber,
-        "code" => $code,
-      ]);
+    try {
+      $client = new Client(getenv("TWILIO_ACCOUNT_SID"), getenv("TWILIO_AUTH_TOKEN"));
+      $verification_check = $client->verify->v2
+        ->services(getenv("TWILIO_VERIFY_SERVICE_SID"))
+        ->verificationChecks->create([
+          "to" => $toPhoneNumber,
+          "code" => $code,
+        ]);
 
-    return $verification_check->valid;
+      return $verification_check->valid;
+    } catch (Exception $e) {
+      throw new Exception($e->getMessage(), code: 424);
+    }
   }
 }
